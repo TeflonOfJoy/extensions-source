4eb57784e28985f62f6ec34f83a3769f62a67bde
Universe <0universe0@protonmail.com>
2024-02-23 12:12:07 +0100

    add 1stkissmanga

---
+++ src/en/kissmanga1st/src/eu/kanade/tachiyomi/en/kissmanga1st/KissManga1st.kt
@@ -0,0 +1,4 @@
+package eu.kanade.tachiyomi.extension.en.kissmanga1st
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+
+class KissManga1st : Madara("1stKissManga", "https://1st-kissmanga.net/", "en")
